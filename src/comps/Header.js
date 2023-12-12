e3d37962ece4ef4cb8f27b328ab60ad6917d438e
hybf1212 <hybf1212@naver.com>
2023-12-12 20:44:34 +0900

    모바일 header, nav 배치

---
+++ src/comps/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { IoCart, IoSearch, IoMenu } from "react-icons/io5";
+
+const Header = () => {
+    return(
+        <>
+        <header id='MO-header'>
+            <div className='MO-top-header bg-white fixed-top'>
+                <img className='MO-top-logo d-block' src={`${process.env.PUBLIC_URL}/img/logo/newbalance-logo-b.svg`} alt="검정색 뉴발란스 로고" />
+            </div>
+            <div className='MO-nav-background fixed-bottom'>
+                <nav className='MO-navbar bg-white d-flex justify-content-around align-items-center'>
+                    <div className='d-flex justify-content-center'>
+                        <button className='MO-nav-search d-block'><IoSearch className='d-block' color='black'/></button>
+                    </div>
+                    <div className='d-flex justify-content-center'>
+                        <button className='MO-nav-menu d-block'><IoMenu className='d-block' color='black'/></button>
+                    </div>
+                    <div className='d-flex justify-content-center'>
+                        <button className='MO-nav-cart d-block'><IoCart className='d-block' color='black'/></button>
+                    </div>
+                </nav>
+            </div>
+
+            
+        </header>
+        </>
+    )
+}
+
+export default Header;
